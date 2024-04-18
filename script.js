bed414620e5948491f5294ac9850e66ee87d4721
deeprathod97 <deeprathod9797@gmail.com>
2024-04-18 20:11:10 +0530

    Variables in javaScript

---
+++ script.js
@@ -0,0 +1,17 @@
+// Variables in javaScript #1
+
+console.log("This vs code contains code for the Ultimat JavaScript ")
+
+// var  a = 67 
+let  a = 67 // a containr 67 
+a = "deep "
+// console.log(a) valu ko print karta he
+console.log(a)
+
+//let 8deep = 7 // Not allowed this will tthrow an error 
+// let var = 7 
+
+
+
+
+
